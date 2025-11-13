e2ec7e05c8c5681eb6526db7658c0ba82b5aae4f
Alan Porto <alanportodeveloper@gmail.com>
2025-11-13 19:32:08 -0300

    "feat: add barbershop item component"

---
+++ lib/prisma.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from "../app/generated/prisma/client";
+
+const globalFormPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma = globalFormPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalFormPrisma.prisma = prisma;
